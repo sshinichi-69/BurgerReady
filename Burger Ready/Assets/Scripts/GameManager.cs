5d69e38d2c1298b203b72d042f8b94e3a053fa28
Khánh <77840594+sshinichi-69@users.noreply.github.com>
2024-08-22 00:11:18 +0700

    Add Exit Game Feature

--- Burger Ready/Assets/Scripts/GameManager.cs
+++ Burger Ready/Assets/Scripts/GameManager.cs
@@ -26,7 +26,18 @@ namespace BurgerReady
         // Update is called once per frame
         void Update()
         {
-
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                if (Application.platform == RuntimePlatform.Android)
+                {
+                    AndroidJavaObject activity = new AndroidJavaClass("com.unity3d.player.UnityPlayer").GetStatic<AndroidJavaObject>("currentActivity");
+                    activity.Call<bool>("moveTaskToBack", true);
+                }
+                else
+                {
+                    Application.Quit();
+                }
+            }
         }
 
         public IEnumerator AddNewCustomer()
